eafb469c40430867bdf313741ba9b661abbe7ca5
João Vitor Ibiapino <ibiapinojoaovitor@gmail.com>
2025-07-10 02:50:37 -0300

    components building

--- src/br/com/dio/ui/custom/panel/SudokuSector.java
+++ src/br/com/dio/ui/custom/panel/SudokuSector.java
@@ -1,5 +1,7 @@
 package br.com.dio.ui.custom.panel;
 
+import br.com.dio.ui.custom.input.NumberText;
+import java.util.List;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
@@ -8,12 +10,13 @@ import static java.awt.Color.black;
 
 public class SudokuSector extends JPanel {
 
-    public SudokuSector(){
+    public SudokuSector(final List<NumberText> textFields){
         var dimension = new Dimension(170, 170);
         this.setSize(dimension);
         this.setPreferredSize(dimension);
         this.setBorder(new LineBorder(black, 2, true));
         this.setVisible(true);
+        textFields.forEach(this::add);
     }
 
 }
